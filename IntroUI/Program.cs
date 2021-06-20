32bdb83ebeb68af4874bdf8b196c1a417a3fcc85
pc-v2 <meguminxkazuma@protonmail.com>
2021-06-20 14:32:47 +0700

    first commit

---
+++ IntroUI/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using IntroLibrary;
+namespace IntroUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PersonModel person = new PersonModel
+            {
+                FirstName = "Jerry",
+                LastName = "Shaw"
+            };
+            System.Console.WriteLine($"{person.FirstName} {person.LastName} is my name");
+            
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("fuck you!");
+        }
+    }
+}
